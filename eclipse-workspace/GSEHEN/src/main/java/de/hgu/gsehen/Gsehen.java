b95a7c94a5d46ad2c0824169fd43665d8854825c
CWI <38039621+de-cgs-online-cwitzke@users.noreply.github.com>
2018-04-10 16:22:45 +0200

    Aufgeräumt. PolyGon nun im FarmView; Code im Controller.

--- eclipse-workspace/GSEHEN/src/main/java/de/hgu/gsehen/Gsehen.java
+++ eclipse-workspace/GSEHEN/src/main/java/de/hgu/gsehen/Gsehen.java
@@ -10,23 +10,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Logger;
-import de.hgu.gsehen.gui.GeoPoint;
-import de.hgu.gsehen.gui.GeoPolygon;
-import de.hgu.gsehen.gui.PolygonData;
 import de.hgu.gsehen.webview.Map;
 import javafx.application.Application;
 import javafx.concurrent.Worker.State;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.paint.Color;
-import javafx.scene.transform.Affine;
-import javafx.scene.transform.NonInvertibleTransformException;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -47,7 +38,7 @@ public class Gsehen extends Application {
   public static final String WEB_VIEW_ID = "#webView";
   public static final String DEBUG_TEXTAREA_ID = "#debugTA";
   public static final String TAB_PANE_ID = "#tabPane";
-
+  
   private static final Logger LOGGER = Logger.getLogger(Gsehen.class.getName());
 
   /**
@@ -99,91 +90,11 @@ public class Gsehen extends Application {
     });
     engine.loadContent(Map.getMapHtml());
 
-    int width  = 1000; // TODO size depending on parent!?
-    int height = 600;  // ====
-    Canvas canvas = new Canvas(width, height);
-    GraphicsContext gc = canvas.getGraphicsContext2D();
-    GeoPolygon[] polygons = {
-        new GeoPolygon(
-            new GeoPoint(52.2, 10.5), new GeoPoint(52.5, 10.5), new GeoPoint(52.4, 10.1)),
-        new GeoPolygon(
-            new GeoPoint(53.2, 10.5), new GeoPoint(53.5, 10.5), new GeoPoint(53.4, 10.1)),
-        new GeoPolygon(
-            new GeoPoint(52.2, 11.5), new GeoPoint(52.5, 11.5), new GeoPoint(52.4, 11.1))
-    };
-    setTransformation(gc, width, height, polygons);
-    drawShapes(gc, polygons);
-
-    Tab canvasTab = new Tab("(programmatisches Canvas-Tab)");
-    canvasTab.setContent(canvas);
     TabPane tabPane = (TabPane) stage.getScene().lookup(TAB_PANE_ID);
     tabPane.getTabs().remove(4);
-    tabPane.getTabs().add(canvasTab);
-    tabPane.getSelectionModel().select(tabPane.getTabs().size() - 1);
-
+    
     //TextArea debugTextArea = (TextArea) stage.getScene().lookup(DEBUG_TEXTAREA_ID);
     //testDatabase(debugTextArea);
-  }
-
-  private void drawShapes(GraphicsContext gc, GeoPolygon... polygons) {
-    gc.setStroke(Color.WHITE);
-    gc.setFill(Color.WHEAT);
-    for (GeoPolygon polygon : polygons) {
-      PolygonData polygonData = polygon.getPolygonData();
-      gc.fillPolygon(polygonData.getPointsX(), polygonData.getPointsY(),
-          polygonData.getPointsCount());
-    }
-  }
-
-  private void setTransformation(GraphicsContext gc, int widthPx, int heightPx,
-      GeoPolygon... polygons) {
-    if (polygons == null || polygons.length == 0) {
-      throw new IllegalArgumentException("at least one polygon must be given");
-    }
-    GeoPolygon g = polygons[0];
-    double minX = g.getMinX();
-    double maxX = g.getMaxX();
-    double minY = g.getMinY();
-    double maxY = g.getMaxY();
-    for (int i = 1; i < polygons.length; i++) {
-      g = polygons[i];
-      double compare = g.getMinX();
-      if (compare < minX) {
-        minX = compare;
-      }
-      compare = g.getMaxX();
-      if (compare < maxX) {
-        maxX = compare;
-      }
-      compare = g.getMinY();
-      if (compare < maxX) {
-        minY = compare;
-      }
-      compare = g.getMaxY();
-      if (compare < maxX) {
-        maxY = compare;
-      }
-    }
-    setTransformation(gc, widthPx, heightPx, minX, maxX, minY, maxY);
-  }
-
-  @SuppressWarnings({"checkstyle:rightcurly"})
-  private void setTransformation(GraphicsContext gc, int widthPx, int heightPx,
-      double minX, double maxX, double minY, double maxY) {
-    double rangeX = maxX - minX;
-    double rangeY = maxY - minY;
-    Affine affineTransformation = new Affine();
-    double ratioX = rangeX / widthPx;
-    double ratioY = rangeY / heightPx;
-    affineTransformation.appendTranslation(minX, minY);
-    affineTransformation.appendScale(ratioX, ratioY);
-    try {
-      affineTransformation.invert();
-    }
-    catch (NonInvertibleTransformException e) {
-      throw new RuntimeException(e);
-    }
-    gc.setTransform(affineTransformation);
   }
 
   @SuppressWarnings({"unused", "checkstyle:rightcurly"})
--- eclipse-workspace/GSEHEN/src/main/resources/de/hgu/gsehen/MainController.java
+++ eclipse-workspace/GSEHEN/src/main/resources/de/hgu/gsehen/MainController.java
@@ -1,14 +1,24 @@
 package de.hgu.gsehen;
 
+import de.hgu.gsehen.gui.GeoPoint;
+import de.hgu.gsehen.gui.GeoPolygon;
+import de.hgu.gsehen.gui.PolygonData;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.transform.Affine;
+import javafx.scene.transform.NonInvertibleTransformException;
 import javafx.stage.Stage;
 
 /**
@@ -34,6 +44,8 @@ public class MainController {
   private Tab fieldPlotViewTab;
   @FXML
   private Tab contactViewTab;
+  @FXML
+  private AnchorPane farmViewAnchor;
 
   // Help-Menu
   @FXML
@@ -43,6 +55,10 @@ public class MainController {
   @FXML
   private MenuItem aboutUsMenuItem;
 
+  public AnchorPane getFarmViewAnchor() {
+    return farmViewAnchor;
+  }
+
   // Hides the Accordion and the tabs in the TabPane.
   @FXML
   protected void openContactView(ActionEvent o) {
@@ -50,7 +66,7 @@ public class MainController {
     tabPane.getTabs().clear();
     tabPane.getTabs().add(contactViewTab);
   }
-  
+
   // Returns to Main-Menu.
   @FXML
   protected void backFromContactView(ActionEvent b) {
@@ -70,6 +86,87 @@ public class MainController {
     stage.setTitle("About us");
     stage.setScene(scene);
     stage.show();
+  }
+
+  @FXML
+  protected void drawPolygon(Event d) {
+    int width = 1000; // TODO size depending on parent!?
+    int height = 600; // ====
+    Canvas canvas = new Canvas(width, height);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+    GeoPolygon[] polygons = {
+        new GeoPolygon(new GeoPoint(52.2, 10.5), new GeoPoint(52.5, 10.5),
+            new GeoPoint(52.4, 10.1)),
+        new GeoPolygon(new GeoPoint(53.2, 10.5), new GeoPoint(53.5, 10.5),
+            new GeoPoint(53.4, 10.1)),
+        new GeoPolygon(new GeoPoint(52.2, 11.5), new GeoPoint(52.5, 11.5),
+            new GeoPoint(52.4, 11.1))};
+    setTransformation(gc, width, height, polygons);
+    drawShapes(gc, polygons);
+    AnchorPane.setTopAnchor(canvas, 40.0);
+    AnchorPane.setLeftAnchor(canvas, 50.0);
+    AnchorPane.setRightAnchor(canvas, 70.0);
+    farmViewAnchor.getChildren().addAll(canvas);
+  }
+
+  private void drawShapes(GraphicsContext gc, GeoPolygon... polygons) {
+    gc.setStroke(Color.WHITE);
+    gc.setFill(Color.WHEAT);
+    for (GeoPolygon polygon : polygons) {
+      PolygonData polygonData = polygon.getPolygonData();
+      gc.fillPolygon(polygonData.getPointsX(), polygonData.getPointsY(),
+          polygonData.getPointsCount());
+    }
+  }
+
+  private void setTransformation(GraphicsContext gc, int widthPx, int heightPx,
+      GeoPolygon... polygons) {
+    if (polygons == null || polygons.length == 0) {
+      throw new IllegalArgumentException("at least one polygon must be given");
+    }
+    GeoPolygon g = polygons[0];
+    double minX = g.getMinX();
+    double maxX = g.getMaxX();
+    double minY = g.getMinY();
+    double maxY = g.getMaxY();
+    for (int i = 1; i < polygons.length; i++) {
+      g = polygons[i];
+      double compare = g.getMinX();
+      if (compare < minX) {
+        minX = compare;
+      }
+      compare = g.getMaxX();
+      if (compare < maxX) {
+        maxX = compare;
+      }
+      compare = g.getMinY();
+      if (compare < maxX) {
+        minY = compare;
+      }
+      compare = g.getMaxY();
+      if (compare < maxX) {
+        maxY = compare;
+      }
+    }
+    setTransformation(gc, widthPx, heightPx, minX, maxX, minY, maxY);
+  }
+
+  @SuppressWarnings({"checkstyle:rightcurly"})
+  private void setTransformation(GraphicsContext gc, int widthPx, int heightPx, double minX,
+      double maxX, double minY, double maxY) {
+    double rangeX = maxX - minX;
+    double rangeY = maxY - minY;
+    Affine affineTransformation = new Affine();
+    double ratioX = rangeX / widthPx;
+    double ratioY = rangeY / heightPx;
+    affineTransformation.appendTranslation(minX, minY);
+    affineTransformation.appendScale(ratioX, ratioY);
+    try {
+      affineTransformation.invert();
+    } catch (NonInvertibleTransformException e) {
+      throw new RuntimeException(e);
+    }
+    gc.setTransform(affineTransformation);
   }
 
 }
