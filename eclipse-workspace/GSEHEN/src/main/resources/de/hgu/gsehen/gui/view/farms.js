580ab42b392c6ffdc6f06bb247f70be86d796fbf
AT <35488642+de-cgs-online-atappe@users.noreply.github.com>
2018-06-07 10:55:06 +0200

    Test mit explizitem Hintergrund

--- eclipse-workspace/GSEHEN/src/main/resources/de/hgu/gsehen/gui/view/farms.js
+++ eclipse-workspace/GSEHEN/src/main/resources/de/hgu/gsehen/gui/view/farms.js
@@ -53,6 +53,8 @@ function lineAndLog(context, points, pointIndex, viewport, factor, canvasHeight)
 function drawPoints(x, y, k) {
   context.save();
   context.clearRect(0, 0, canvasWidth, canvasHeight);
+  context.fillStyle = "#FFDDDD";
+  context.fillRect(0, 0, canvasWidth, canvasHeight);
   context.translate(x, y);
   context.scale(k, k);
   var drawables = webController.getDrawables();
